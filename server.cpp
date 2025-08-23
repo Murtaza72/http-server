ecce37c2b37e05298059be8fb25074c18ec5fd76
Murtaza72 <murtazatuta72@gmail.com>
2025-08-23 21:08:01 +0300

    Add cmd args and ignore query string

--- server.cpp
+++ server.cpp
@@ -1,77 +1,3 @@
-/*
-
-Credit: u/Drach88
-https://www.reddit.com/r/C_Programming/comments/kbfa6t/comment/gfh8kid/
-
-In terms of HTTP, the only resource you need is the HTTP standard itself. https://tools.ietf.org/html/rfc1945
-
-For now, you're only going to concern yourself with:
-
-What constitutes a properly-formatted request.
-
-The conditions that would result in following properly-formatted responses: 200 OK, 400 Bad Request, 403 Forbidden,
-404 Not Found, 500 Internal Service Error, and 501 Not Implemented (These are the codes you're going to implement)
-
-For now, ignore everything related to headers other than noting that headers exist.
-
-In terms of the sockets stuff, use Beej's guide. https://beej.us/guide/bgnet/. At its simplest, your program needs to:
-
-    1. Create a socket
-
-    2. Bind the socket to an address
-
-    3. Listen on the address
-
-    4. Block on Accept until a connection is made
-
-    5. Read on the connected socket
-
-    6. Figure out how to respond
-
-    7. Write back on the connected socket
-
-    8. Close the connection
-
-    9. Go back to blocking on Accept
-
-That out of the way, here's a quick checklist to give your project a little structure:
-
-    1. Write a program that accepts a connection on a port (specify the port number as a command line argument), and
-    immediately sends back a dummy HTTP 1.0 "200 OK" response, along with a dummy minimal HTML-encoded message before
-    closing the connection. For the entire project, you're going to respond with HTTP 1.0 responses regardless of what
-    version of the request you receive. Test this using netcat, then try it using a web browser.
-
-    2. Modify your program to parse the request. You can ignore all of the headers for now. For now, you're only
-    responding to validly formatted GET requests. Send the dummy message back for any validly formatted GET requests. If
-    the request is improperly formatted, respond 400. For any other valid requests apart from GET requests, respond with
-    501.
-
-    3. Modify your program to take another command line argument for the "root" directory. Make a directory somewhere,
-    and put a dummy HTML file called index.html and another dummy HTML file called whatever you want. Add a dummy image
-    file as well. When your server starts up, verify that the folder exists and that your program has permissions to
-    view the contents. Modify your program to parse the path from valid GET requests. Upon parsing the path, check the
-    root folder to see if a file matches that filename. If so, respond 200, read the file and write the file to the
-    client socket. If the path is "/" (ie. the root directory) serve index.html. If the requested file does not exist,
-    respond 404 not found. Make sure your solution works for text files as well as binaries (ie. images).
-
-    4. Add a couple of folders to your root folder, and add dummy html files (and dummy index.html files) to them. Add a
-    few levels of nested folders. Modify your program to improve the path-parsing logic to handle folders, and handle
-    responses appropriately.
-
-
-    -----------------------------------------------------------------------------------------------------------------
-
-
-    5. Modify the permissions on a few dummy folders and files to make their read permissions forbidden to your server
-    program. Implement the 403 response appropriately. Scrutinize the URI standard, and modify your path-parsing to
-    strip out (and handle) any troublesome characters. Modify your path-parsing to handle (ignore) query strings and
-    fragments. (? and #). Modify your path-parsing to ensure it does not allow malicious folder navigation using "..".
-
-There's plenty more you can do from there, but that breaks down the project into bite-sized pieces to get you to a
-bare-minimum HTTP server.
-
-*/
-
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -86,7 +12,7 @@ bare-minimum HTTP server.
 #include <string>
 #include <vector>
 
-#define BUFFER_SIZE 1024 * 4
+#define BUFFER_SIZE 1024 * 10
 #define PORT 8080
 #define ADDR INADDR_ANY
 #define BACKLOG 10
@@ -235,18 +161,29 @@ std::string normalize_path(std::string urlPath, std::string root)
     return normalized;
 }
 
-std::string normalize_uri(std::string uri)
+std::string remove_params(std::string uri)
+{
+    int pos;
+    if ((pos = uri.find("?")) != std::string::npos)
+    {
+        uri = uri.substr(0, pos);
+    }
+
+    return uri;
+}
+
+std::string normalize_uri(std::string uri, std::string root)
 {
     std::string decoded = decode_uri(uri);
-    std::string normalized = normalize_path(decoded, ROOT);
+    std::string normalized = normalize_path(remove_params(decoded), root);
 
     return normalized;
 }
 
-bool is_valid_uri(std::string uri, std::string& valid_uri)
+bool is_valid_uri(std::string uri, std::string root, std::string& valid_uri)
 {
     // TODO: Implement logic to check if the uri is valid
-    std::string normalized_uri = normalize_uri(uri);
+    std::string normalized_uri = normalize_uri(uri, root);
     valid_uri = normalized_uri;
 
     return true;
@@ -270,14 +207,16 @@ std::string response_bad_request() { return "HTTP/1.1 400 Bad Request\r\n"; }
 
 std::string response_not_found() { return "HTTP/1.1 404 Not Found\r\n"; }
 
+std::string response_forbidden() { return "HTTP/1.1 403 Forbidden \r\n"; }
+
 int is_directory(std::string path)
 {
     struct stat path_stat;
     stat(path.c_str(), &path_stat);
-    return !S_ISREG(path_stat.st_mode);
+    return S_ISDIR(path_stat.st_mode);
 }
 
-std::string parse_req(char* raw_buffer)
+std::string parse_req(char* raw_buffer, std::string root)
 {
     std::string buffer(raw_buffer);
 
@@ -286,6 +225,8 @@ std::string parse_req(char* raw_buffer)
     std::vector<std::string> request_tokens = split(buffer, "\r\n");
 
     std::string response;
+
+    std::cout << "----------\n" << buffer << std::endl;
 
     for (int i = 0; i < request_tokens.size(); i++)
     {
@@ -303,7 +244,7 @@ std::string parse_req(char* raw_buffer)
             }
 
             std::string valid_uri;
-            if (!is_valid_uri(request_line.at(1), valid_uri) || !is_valid_http_version(request_line.at(2)))
+            if (!is_valid_uri(request_line.at(1), root, valid_uri) || !is_valid_http_version(request_line.at(2)))
             {
                 current_state = state::error;
                 response = response_bad_request();
@@ -340,28 +281,21 @@ std::string parse_req(char* raw_buffer)
                 i++;
             }
             i--;
-
-            for (auto& p : request.headers)
-            {
-                std::cout << p.first << ": " << p.second << std::endl;
-            }
         }
     }
 
-    std::cout << std::endl << "Path = " << request.request_line.path << std::endl << std::endl;
-
     if (current_state != state::error)
     {
-        // check if the path is a directory
-        // if true, add /index.html to the path
-        if (is_directory(request.request_line.path))
+        if (access(request.request_line.path.c_str(), R_OK) == 0)
         {
-            request.request_line.path += "/index.html";
-        }
+            // check if the path is a directory
+            // if true, add /index.html to the path
+            if (is_directory(request.request_line.path))
+            {
+                request.request_line.path += "/index.html";
+            }
 
-        std::ifstream file(request.request_line.path);
-        if (file.good())
-        {
+            std::ifstream file(request.request_line.path);
             std::string str;
             std::string file_contents;
             while (std::getline(file, str))
@@ -371,7 +305,13 @@ std::string parse_req(char* raw_buffer)
 
             response = attach_response_headers(file_contents);
         }
-        else
+        else if (errno == EACCES)
+        {
+            // errno is global variable set by the syscalls
+            // server doesn't have permission to read
+            response = response_forbidden();
+        }
+        else if (errno == ENOENT)
         {
             response = response_not_found();
         }
@@ -380,8 +320,24 @@ std::string parse_req(char* raw_buffer)
     return response;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <IP> <PORT> <ROOT_DIR>" << std::endl;
+        exit(1);
+    }
+
+    std::string ip = argv[1];
+    int port = std::stoi(argv[2]);
+    std::string root = argv[3];
+
+    if (port < 1 || port > 65535)
+    {
+        std::cerr << "PORT should be in range 1 to 65535." << std::endl;
+        exit(1);
+    }
+
     std::cout << "Server started..." << std::endl;
 
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -398,8 +354,12 @@ int main()
     struct sockaddr_in addr;
     std::memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = htons(ADDR);
+    addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip.c_str(), &addr.sin_addr) <= 0)
+    {
+        std::cerr << "inet_pton" << std::endl;
+        exit(1);
+    }
 
     if (bind(socket_fd, (struct sockaddr*)&addr, sizeof(addr)) != 0)
     {
@@ -427,9 +387,8 @@ int main()
 
         std::memset(&buffer, 0, sizeof(buffer));
         int len = recv(client_fd, buffer, BUFFER_SIZE, 0);
-        std::cout << buffer << std::endl;
 
-        std::string response = parse_req(buffer);
+        std::string response = parse_req(buffer, root);
 
         send(client_fd, response.c_str(), sizeof(char) * response.length(), 0);
 
